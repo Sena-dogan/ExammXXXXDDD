cce54de3996b3b68e9a68786b6e2085ead2a33f5
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2023-05-09 17:31:44 +0300

    bumblebeeee

--- Level4/ft_split.c
+++ Level4/ft_split.c
@@ -1,125 +1,87 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-char *ft_strncpy(char *s1, char *s2, int n)
+int ft_strlen(char *str)
 {
-	int i = -1;
+    int i = 0;
 
-	while (++i < n && s2[i])
-		s1[i] = s2[i];
-	s1[i] = '\0';
-	return (s1);
+    while (str[i] != '\0')
+        i++;
+    return (i);
 }
 
 int word_counter(char *str)
 {
     int i = 0;
     int wc = 0;
+    int len = ft_strlen(str);
+
+    while (i < len)
+    {
+        while ((str[i] == 32 || str[i] == 9 || str[i] == 10) && i < len)
+            i++;
+        if (i < len)
+            wc++;
+        while ((str[i] != 32 && str[i] != 9 && str[i] != 10) && i < len)
+            i++;
+    }
+    return (wc);
+}
+
+char *ft_substr(int start, int end, char *str)
+{
+    int i = 0;
+    char *word = malloc(sizeof(char) * (end - start) + 1);
+    while (str[start] && start < end)
+    {
+        word[i] = str[start];
+        start++;
+        i++;
+    }
+    word[i] = '\0';
+    return(word);
+}
+
+char    **ft_split(char *str)
+{
+    int i = 0;
+    int j = 0;
+    int wc = word_counter(str);
+    int len = ft_strlen(str);
+    char **bee = malloc(sizeof(char *) * wc + 1);
+    
+    while (i < len)
+    {
+        while ((str[i] == 32 || str[i] == 9 || str[i] == 10) && i < len)
+            i++;
+        int first_w = i;
+        while ((str[i] != 32 && str[i] != 9 && str[i] != 10) && i < len)
+            i++;
+        int last_w = i;
+        if (last_w > first_w) // i kelimenin son karakteri, w kelimenin ilk karakteri
+        {
+            bee[j] = malloc(sizeof(char) * (last_w - first_w) + 1);
+            if (last_w > first_w)
+            {
+                bee[j] = ft_substr(first_w, last_w, str);
+                j++;
+            }
+        }
+    }
+    bee[j] = NULL;
+    return(bee);
+}
+
+
+int main(int ac, char **av)
+{
+    char *s = av[1];
+    int i = 0;
+    char **str = ft_split(s);
 
     while (str[i])
-	{
-		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
-			i++;
-		if (str[i])
-			wc++;
-		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
-			i++;
-	}
-    return(wc);
+    {
+        printf("%s\n", str[i]);
+        i++;
+    }
 }
-
-char	**ft_split(char *str)
-{
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int wc = word_counter(str);
-	
-	char **res = (char **)malloc(sizeof(char *) * (wc + 1));
-	i = 0;
-	while (str[i])
-	{
-		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
-			i++;
-		j = i;
-		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
-			i++;
-		if (i > j)
-		{
-			res[k] = (char *)malloc(sizeof(char) * ((i - j) + 1));
-			ft_strncpy(res[k++], &str[j], i - j);
-		}
-	}
-	res[k] = '\0';
-	return (res);
-}
-
-
-// int	wordcounter(const char *s)
-// {
-// 	int	i = 0;
-//     int x = 0;
-
-// 	while (*s)
-// 	{
-// 		while ((s[i] == ' ' || s[i] == '\t' || s[i] == '\n') && s[i])
-// 			i++;
-// 		if (s[i] == '\0')
-// 			return (x);
-// 		while (*s != (s[i] == ' ' || s[i] == '\t' || s[i] == '\n') && *s)
-// 			i++;
-// 		x++;
-// 	}
-// 	return (x);
-// }
-
-// int	charcounter(const char *s)
-// {
-//     int i = 0;
-// 	int	x = 0;
-
-// 	while (s[i] && !(s[i] == ' ' || s[i] == '\t' || s[i] == '\n'))
-// 	{
-// 		x++;
-// 		i++;
-// 	}
-// 	return (x);
-// }
-
-// char	**ft_split(const char *s)
-// {
-// 	char	**res;
-// 	int		resi;
-
-// 	if (!s)
-// 		return (0);
-// 	resi = 0;
-// 	res = malloc (sizeof(char *) * wordcounter(s, c) + 1);
-// 	if (!res)
-// 		return (0);
-// 	while (*s)
-// 	{
-// 		while (*s == c && *s)
-// 			s++;
-// 		if (*s == '\0')
-// 			break ;
-// 		res[resi] = ft_substr(s, 0, charcounter(s, c));
-// 		resi++;
-// 		s = s + charcounter(s, c);
-// 	}
-// 	res[resi] = NULL;
-// 	return (res);
-// }
-
-// int main(int ac, char **av)
-// {
-//     char *s = av[1];
-//     int i = 0;
-//     char **str = ft_split(s);
-
-//     while (str[i])
-//     {
-//         printf("%s\n", str[i]);
-//         i++;
-//     }
-// }
